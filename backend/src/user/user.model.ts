792814a83cdbe01f3ad49d44938f8f5ea91a545f
raheesamumthaz <r4raheesamumthaz@gmail.com>
2024-01-30 22:32:07 -0800

    backend  in nest js added

---
+++ backend/src/user/user.model.ts
@@ -0,0 +1,15 @@
+import * as mongoose from 'mongoose';
+
+export const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+export interface User extends mongoose.Document {
+  email: string;
+  name: string;
+  password: string;
+}
+
+export const UserModel = mongoose.model<User>('User', UserSchema);
